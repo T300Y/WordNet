406e7272340acc2d75881b871d82e55f127c6f58
T300Y <86957414+T300Y@users.noreply.github.com>
2023-08-30 15:44:40 +1000

    Fixed bugs with letter edge bouncing and now draws circle around selected letter

--- main.py
+++ main.py
@@ -22,27 +22,37 @@ class TextSprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.font_size = font_size
         self.rect.topleft = (x, y)
-        self.velocity = pygame.Vector2(3, 3)  # Fixed velocity
+        self.velocity = pygame.Vector2(1.5, 1.5)  # Fixed velocity
         self.width = screen_width
         self.height = screen_height
         self.draw_circle = False  # Flag to indicate if circle should be drawn
+
 
     def update(self):
         self.rect.x += self.velocity.x
         self.rect.y += self.velocity.y
 
-        if self.rect.left < 0 or self.rect.right > self.width:
+        # Prevent going beyond screen edges
+        if self.rect.left < 0:
+            self.rect.left = 0
             self.velocity.x *= -1
-        if self.rect.top < 0 or self.rect.bottom > self.height:
+        if self.rect.right > self.width:
+            self.rect.right = self.width
+            self.velocity.x *= -1
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.velocity.y *= -1
+        if self.rect.bottom > self.height:
+            self.rect.bottom = self.height
             self.velocity.y *= -1
 
-        collide_list = pygame.sprite.spritecollide(self, all_sprites, False)
-        for sprite in collide_list:
-            if sprite != self:
-                if self.check_collision(sprite):
-                    self.resolve_collision(sprite)
-                    self.rect.move_ip(-self.velocity.x, -self.velocity.y)
-                break
+        #collide_list = pygame.sprite.spritecollide(self, all_sprites, False)
+        #for sprite in collide_list:
+            #if sprite != self:
+              #  if self.check_collision(sprite):
+                 #   self.resolve_collision(sprite)
+                  #  self.rect.move_ip(-self.velocity.x, -self.velocity.y)
+              #  break
 
     def check_collision(self, other_sprite):
         axes = [self.velocity.normalize(), pygame.Vector2(-self.velocity.y, self.velocity.x).normalize()]
@@ -68,7 +78,7 @@ class TextSprite(pygame.sprite.Sprite):
         collision_normal = pygame.Vector2(other_sprite.rect.center) - pygame.Vector2(self.rect.center)
         collision_normal = collision_normal.normalize()
         impulse = 2 * relative_velocity.dot(collision_normal)
-        self.velocity -= impulse * collision_normal
+        self.velocity = self.velocity
         other_sprite.velocity += impulse * collision_normal
 
 def draw_window():
@@ -78,7 +88,6 @@ def draw_window():
     for sprite in all_sprites:
         if sprite.draw_circle:
             pygame.draw.circle(WIN, (255,255,255), sprite.rect.center, 50, 2)
-            sprite.draw_circle = False  # Reset the flag
     pygame.display.flip()
 
 def main():
@@ -87,6 +96,7 @@ def main():
 
     clock = pygame.time.Clock()
     run = True
+    selected_sprite = None  # Variable to keep track of the selected sprite
 
     while run:
         clock.tick(FPS)
@@ -98,7 +108,12 @@ def main():
                 mouse_position = pygame.mouse.get_pos()
                 for sprite in all_sprites:
                     if sprite.rect.collidepoint(mouse_position):
-                        sprite.draw_circle = True  # Set the flag to draw circle
+                        if selected_sprite is not None:
+                            selected_sprite.draw_circle = False  # Deselect the previously selected sprite
+                        sprite.draw_circle = True  # Set the flag to draw circle for the clicked sprite
+                        selected_sprite = sprite  # Update the selected sprite
+                    else:
+                        sprite.draw_circle = False
 
         if len(array_of_sprites) < 15:
             array_of_sprites, no_active_letters = letter_generator(array_of_sprites, no_active_letters)
@@ -114,8 +129,8 @@ def letter_generator(array_of_sprites, no_of_sprites):
     global all_sprites
 
     while True:
-        x = random.randint(0, WIDTH - 36)
-        y = random.randint(0, HEIGHT - 36)
+        x = random.randint(50, WIDTH - 50)  # Adjusted the range to ensure some padding from the edges
+        y = random.randint(50, HEIGHT - 50)  # Adjusted the range to ensure some padding from the edges
         text_sprite = TextSprite(random_letter(), 36, (255, 0, 0), x, y, WIDTH, HEIGHT)
         
         # Check for collisions with existing sprites
